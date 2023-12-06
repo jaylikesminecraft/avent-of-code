437741a78876cadef9b1cc93362eaaa0c1c11804
JayLiuTU <tuj76761@temple.edu>
2023-12-05 23:01:33 -0500

    answer 54251 not correct idk why

--- day 2/day2.py
+++ day 2/day2.py
@@ -6,39 +6,48 @@ with open("./day 2/list.txt", 'r') as list:
 
 strings = x.split()
 
-regexs = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+#regexs = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-regexs = r"(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)"
+regexs = r"one|two|three|four|five|six|seven|eight|nine|\d+"
 
 sum = 0
 
+def wordToInt(word):
+    word = str(word)
+    match word:
+        case "one":
+            return "1"
+        case "two":
+            return "2"
+        case "three":
+            return "3"
+        case "four":
+            return "4"
+        case "five":
+            return "5"
+        case "six":
+            return "6"
+        case 'seven':
+            return "7"
+        case "eight":
+            return "8"
+        case "nine":
+            return "9"
+        case _:
+            return word
 
 
 for string in strings:
-    numbers = re.findall(regexs, string)
-    print(numbers)
+
+    numbersList = ""
+    allNumbers = re.findall(regexs, string)
+    for word in allNumbers:
+        numbersList += wordToInt(word)
+    sum += int(numbersList[0] + numbersList[len(numbersList)-1])
+
+print(sum)
 
 
 
 
 
-def wordToInt(word: str):
-    match word:
-        case "one":
-            return 1
-        case "two":
-            return 2
-        case "three":
-            return 3
-        case "four":
-            return 4
-        case "five":
-            return 5
-        case "six":
-            return 6
-        case 'seven':
-            return 7
-        case "eight":
-            return 8
-        case "nine":
-            return 9
